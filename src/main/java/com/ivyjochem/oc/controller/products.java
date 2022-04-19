fa425d92c43366c0794d27f6a6d0f2fe431df08a
ScarletIvy <77377889+ScarletIvy@users.noreply.github.com>
2022-04-19 16:38:14 -0500

    the products page now accesses the database, now i need to get the product listings working

--- src/main/java/com/ivyjochem/oc/controller/products.java
+++ src/main/java/com/ivyjochem/oc/controller/products.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
 /**
  * The type Products.
