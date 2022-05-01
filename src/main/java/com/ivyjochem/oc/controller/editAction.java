1e5f7bb1b3b2edfed612c0e8affc87fca4e19621
ScarletIvy <77377889+ScarletIvy@users.noreply.github.com>
2022-05-01 14:16:46 -0500

    Working on edit profile and adding to cart

--- src/main/java/com/ivyjochem/oc/controller/editAction.java
+++ src/main/java/com/ivyjochem/oc/controller/editAction.java
@@ -1,11 +1,14 @@
 package com.ivyjochem.oc.controller;
 
+import com.ivyjochem.oc.controller.entity.user;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -15,8 +18,15 @@ import java.io.IOException;
 public class editAction extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        user oldUser = (user) session.getAttribute("loggedUser");
+        user newUser = (user) session.getAttribute("loggedUser");
         RequestDispatcher dispatcher = request.getRequestDispatcher("home");
-        dispatcher.forward(request, response);
+        if(oldUser == newUser){
+            dispatcher.forward(request, response);
+        } else {
+            dispatcher.forward(request, response);
+        }
     }
 }
 
--- src/main/webapp/cart.jsp
+++ src/main/webapp/cart.jsp
@@ -1,3 +1,5 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <%--
   Created by IntelliJ IDEA.
   User: Ivy
@@ -16,7 +18,21 @@
 <jsp:include page="includes/header.jsp"/>
 <jsp:include page="includes/nav.jsp"/>
 <h2>Cart</h2>
-
+<form class="form-horizontal" action="checkout" method="post">
+<c:choose>
+    <c:when test="${cart.size() == 0}">
+        <h3>Your cart is empty</h3>
+    </c:when>
+    <c:otherwise>
+        <h3>Your cart is not empty</h3>
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="checkout"></label>
+        <div class="col-md-4">
+            <button id="checkout" type="submit" name="checkout" class="btn btn-primary">Check Out</button>
+        </div>
+    </div>
+    </c:otherwise>
+</c:choose>
 <jsp:include page="includes/footer.jsp"/>
 </body>
 </html>
